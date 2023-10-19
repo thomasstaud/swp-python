9e25195fd4ada986b94ebd894bab740e974a9efb
Thomas Staud <thstaud@tsn.at>
2023-10-19 07:58:31 +0200

    kw41 update

--- kw41/main.py
+++ kw41/main.py
@@ -1,24 +1,27 @@
 import random
 
 
-# functions for all 10 pairs
+# TODO: functions for all 10 pairs
 
 
-def poker_stats(cards_num, colors, symbols):
-    # generate random cards
-    cards = []
-    for _ in range(cards_num):
-        card = random.randint(0, (colors*symbols)-1)
-        cards.append(card)
-
+def poker_draw(draw, colors, symbols):
+    print(draw)
     # check for combinations
 
 
+def poker_stats(cards_num, colors, symbols):
+    # generate cards
+    cards = list(range(0, colors*symbols))
+
+    for _ in range(iterations):
+        random.shuffle(cards)
+        poker_draw(cards[:cards_num], colors, symbols)
+
+
 if __name__ == "__main__":
+    iterations = 3
     cards_num = 5
     colors = 4
     symbols = 13
-    iterations = 1000
 
-    for _ in range(iterations):
-        poker_stats(cards_num, colors, symbols)
+    poker_stats(cards_num, colors, symbols)
