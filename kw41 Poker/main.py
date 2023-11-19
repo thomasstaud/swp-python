a4987d1f35cc253f98edbd58772bb5f0247ae81d
Thomas Staud <thstaud@tsn.at>
2023-11-19 15:06:17 +0100

    kw46 poker: args + unittest

--- kw41 Poker/main.py
+++ kw41 Poker/main.py
@@ -1,3 +1,5 @@
+import sys
+
 import poker_stats
 import poker_chart
 
@@ -6,7 +8,10 @@ def main():
     draw_count = 5
     color_count = 4
     symbol_count = 13
+
     iterations = 100000
+    if len(sys.argv) > 1:
+        iterations = int(sys.argv[1])
 
     stats = poker_stats.get_stats(draw_count, color_count, symbol_count, iterations)
     print(stats)
---
+++ kw41 Poker/test.py
@@ -0,0 +1,28 @@
+import unittest
+import poker_combinations
+
+
+class TestPoker(unittest.TestCase):
+
+    def test_straight_flush_normal(self):
+        draw = [0, 1, 2, 3, 4]
+        combination = poker_combinations.check_combinations(draw, 13)
+        self.assertEqual(combination, "straight-flush")
+
+    def test_straight_flush_ace(self):
+        draw = [0, 1, 2, 3, 12]
+        combination = poker_combinations.check_combinations(draw, 13)
+        self.assertEqual(combination, "straight-flush")
+
+    def test_four_of_a_kind(self):
+        draw = [0, 13, 26, 39, 40]
+        combination = poker_combinations.check_combinations(draw, 13)
+        self.assertEqual(combination, "four-of-a-kind")
+
+
+def main():
+    pass
+
+
+if __name__ == "main":
+    main()
